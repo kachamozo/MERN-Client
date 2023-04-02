9b902a83bc761f0a3b891696ff0c195f53d8280a
kachamozo <cachamocito@yahoo.com>
2023-04-02 10:54:43 -0400

    reordenando fetch backend

--- src/Products/components/ListProducts.jsx
+++ src/Products/components/ListProducts.jsx
@@ -1,25 +1,8 @@
-import { useEffect, useState } from 'react';
-import { getProducts } from '../services';
-import Loading from './Loading';
-
-const ListProducts = () => {
-	const [isLoading, setIsLoading] = useState(true);
-	const [products, setProducts] = useState([]);
-
-	useEffect(() => {
-		async function loadProducts() {
-			const response = await getProducts();
-			if (response.status === 200) setProducts(response.data.products);
-			setIsLoading(false);
-		}
-		loadProducts();
-	}, []);
-
-	if (isLoading) return <Loading />;
+const ListProducts = ({ products }) => {
 	return (
 		<>
 			{products.length ? (
-				'mostrar resultados'
+				'Productos: ' + products.length
 			) : (
 				<h2 className='title has-text-centered'>No existen productos</h2>
 			)}
--- src/Products/components/ProductsLayout.jsx
+++ src/Products/components/ProductsLayout.jsx
@@ -1,23 +1,37 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import AddButton from './AddButton';
 import Header from './Header';
 import ListProducts from './ListProducts';
 import { Modal } from 'react-bulma-components';
 import Form from './Form';
-import { saveProduct } from '../services';
+import Loading from './Loading';
+import { saveProduct, getProducts } from '../services';
 
 const ProductLayout = () => {
 	const [isOpenModal, setIsOpenModal] = useState(false);
+	const [isLoading, setIsLoading] = useState(true);
+	const [products, setProducts] = useState([]);
 
-	const handleSubmit = (data) => {
-		saveProduct(data);
+	useEffect(() => {
+		async function loadProducts() {
+			const response = await getProducts();
+			if (response.status === 200) setProducts(response.data.products);
+			setIsLoading(false);
+		}
+		loadProducts();
+	}, [isOpenModal]);
+
+	const handleSubmit = async (data) => {
+		await saveProduct(data);
+		setIsOpenModal(false);
 	};
 
+	if (isLoading) return <Loading />;
 	return (
 		<>
 			<Header title='Products App' />
 			<AddButton onClick={() => setIsOpenModal(true)} />
-			<ListProducts />
+			<ListProducts products={products} />
 			<Modal show={isOpenModal} onClose={() => setIsOpenModal(false)}>
 				<Modal.Card>
 					<Modal.Card.Header>
